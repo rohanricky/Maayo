5a6572c73a6d06918d72aff44d61dd4f7d7400e3
rohanricky <rohanricky0609@gmail.com>
2017-12-29 17:55:54 +0530

    xx

--- background.js
+++
@@ -1,8 +0,0 @@
-const circle="<div id="circle" style="
-  background: blue;
-  width:50px;
-  height:50px;
-  border-radius: 50%;
-  position: absolute;
-
-  bottom: 20;right: 20;"><h3>Hi</h3></div>";
