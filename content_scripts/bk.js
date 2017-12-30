19182d0a131497aacbcf0fa63d605bd5e3dac0c8
rohanricky <rohanricky0609@gmail.com>
2017-12-30 09:33:42 +0530

    believer

--- content_scripts/bk.js
+++ content_scripts/bk.js
@@ -1,12 +1,16 @@
 
 var header = document.createElement("div");
-header.setAttribute("id","MyNameIsRohan")
+header.setAttribute("id","SomethingUnique")
 document.body.appendChild(header);
 var h1=document.createElement("h1");
 h1.setAttribute("id","demo");
-var textbox = document.createElement("input");
-textbox.setAttribute("id","Asura");
+var textbox = document.createElement("div");
+textbox.setAttribute("class","talk-bubble tri-right border btm-right-in");
+textbox.innerHTML='<div class="talktext">\
+  <input type="text" />\
+</div>';
+document.body.appendChild(textbox);
 
-document.getElementById("MyNameIsRohan").addEventListener("click",function(){
-  document.getElementById("Asura").style.display='block';
+document.getElementById("SomethingUnique").addEventListener("click",function(){
+    document.getElementById('talk-bubble').style.display='inline-block';
 });
