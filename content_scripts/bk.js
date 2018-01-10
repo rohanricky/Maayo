bfe685f870062c83b09111ee96e6a0074ef48a4c
rohanricky <rohanricky0609@gmail.com>
2018-01-10 21:20:31 +0530

    onclick

--- content_scripts/bk.js
+++ content_scripts/bk.js
@@ -5,12 +5,31 @@ document.body.appendChild(header);
 var h1=document.createElement("h1");
 h1.setAttribute("id","demo");
 var textbox = document.createElement("div");
-textbox.setAttribute("class","talk-bubble tri-right border btm-right-in");
+textbox.setAttribute("id","talk-bubble");
+textbox.setAttribute("class","tri-right border btm-right-in");
 textbox.innerHTML='<div class="talktext">\
-  <input type="text" />\
+  <input id="input" type="text" onkeypress="return onEnter(event);" />\
 </div>';
 document.body.appendChild(textbox);
 
 document.getElementById("SomethingUnique").addEventListener("click",function(){
-    document.getElementById('talk-bubble').style.display='inline-block';
+    var display=document.getElementById('talk-bubble');
+    display.style.display=='none' ? display.style.display='block' : display.style.display='none';
 });
+
+function onEnter(e){
+  if(e.keyCode == 13 || e.which == 13){
+    alert(document.getElementById("input").value);
+    return false;
+  }
+  return true;
+}
+/*
+$("#SomethingUnique").click(function(){
+  $("#talk-bubble").css("display")=='none'?$("#talk-bubble").css("display","block"):$("#talk-bubble").css("display","none");
+});
+//$('head').append('<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>')
+$(document).click(function(){
+  $("#talk-bubble").hide();
+  });
+*/
